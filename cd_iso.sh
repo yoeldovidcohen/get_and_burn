a713717816f74bdb3781e84dd0fbbd4193ebb934
Yoel Dovid Cohen <ydcohen@localhost.localdomain>
2019-07-08 00:46:14 +0100

    a

--- cd_iso.sh
+++ cd_iso.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-wget $1
+# echo -n "URL: "
+# read URL
+# echo -n "NAME: "
+# read NAME
+# echo -n "SHA1: "
+# read SHA1
+# #echo -n "SHA224: "
+# #read SHA224
+# echo -n "SHA256: "
+# read SHA256
+
+
+# if [SHA1]
+then
+
+
+
 
 wodim -eject -tao speed-2 dev=/dev/sr0 -v -data 
